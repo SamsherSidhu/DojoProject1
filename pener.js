ac464d909726f2bf3fe91d858f6a53cad7aeb262
Samsher Sidhu <titanstrykr442@gmail.com>
2021-03-02 23:29:32 -0500

    boonk gang update

--- pener.js
+++ pener.js
@@ -1,5 +1,11 @@
 function gotosite() {
-    for(let i = 0;i < 100;i++){
-        alert("you're an idiot " + i);
+    for(let i = 1;i < 100;i++){
+        if(i != 69){
+            alert("boonk gang x" + i);
+        }else{
+            alert("boonk gang x" + i + " (nice!)");
+        }
     }
+    alert("ok im done");
 }
+console.log("look at this idiot");
