f6268202e226211caece15a7153a54955348be7b
Samsher Sidhu <titanstrykr442@gmail.com>
2021-04-04 15:35:38 -0400

    for sarah

--- pener.js
+++ pener.js
@@ -1,9 +1,9 @@
 function gotosite() {
     for(let i = 1;i < 100;i++){
         if(i != 69){
-            alert("boonk gang x" + i);
+            alert("AHHHEYUL AHHHHEYUL x" + i);
         }else{
-            alert("boonk gang x" + i + " (nice!)");
+            alert("(nice!)");
         }
     }
     alert("ok im done");
